5e7ca258b196f02dc4dec33e7eac43ce84b33e57
e-qui-nox <yamuna.pixie@gmail.com>
2018-10-03 19:46:48 +0530

    Big Multiplication

---
+++ dc10.cpp
@@ -0,0 +1,20 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ll t;
+    cin >> t;
+    while (t--){
+        string x,y;
+        cin >> x >> y;
+        ll m = 0,n=0;
+        for (ll i=0;i<x.size();i++){
+            m += x[i];
+        }
+        for (ll i=0;i<y.size();i++){
+            n += y[i];
+        }
+        cout << (m%3 * n%3)%3 << endl;
+    }
+    return 0;
+}
